5b6e21f5aa20c96aa044f274c687f1579d43cf7a
Thomas Roberts <45372296+blueberry64@users.noreply.github.com>
2023-01-06 11:32:49 -0800

    cleaned up tests

--- test/ecs_crud_test.js
+++ test/ecs_crud_test.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
+const chai  = require("chai")
+
 const {World} = require("../build/ecs/world");
 const {Component} = require("../build/ecs/component");
+const {deepEqual} = require("assert");
 
 class Position extends Component {
     x = 1;
@@ -15,29 +18,41 @@ class Velocity extends Component{
 }
 
 class Color extends Component{
-    color = "red";
+    constructor(label) {
+        super();
+        this.color = label;
+    }
 }
 
 describe('Create Entity', function () {
 
     let world = new World();
 
-    world.createEntity([new Color(), new Position(), new Velocity()])
-    world.createEntity([new Position, new Velocity(), new Color()]);
+    world.createEntity([new Color("red"), new Position(), new Velocity()])
+    world.createEntity([new Position, new Velocity(), new Color("red")]);
 
     it ("should have one chunk", function (){
         let numChunks = world.chunks.length;
-        console.assert(numChunks === 1);
+        assert(numChunks === 1);
     });
 
     it('both should have position {1, 2, 3}, velocity {4, 5, 6}, color:red', function () {
         let components = world.chunks[0].components;
-        console.log(components["Position"]);
-        console.log(components["Velocity"]);
-        console.log(components["Color"]);
+        let positions  = components["Position"];
+        let velocities = components["Velocity"];
+        let colors     = components["Color"];
 
-        console.assert(components.length === 2);
+        assert(positions.length !== 0)
+        assert(positions.length === velocities.length);
+        assert(positions.length === colors.length);
 
+        let testComponents = (componentArray) => {
+            chai.expect(componentArray[0]).to.eql(componentArray[1]);
+        }
+
+        testComponents(positions);
+        testComponents(velocities);
+        testComponents(colors);
     });
 
 });
